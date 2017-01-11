933e02525e231a9256ae88d83493389b4e72a0ac
Philip Putnam <putnamp989@gmail.com>
2017-01-11 09:08:02 -0800

    add jsquery variables to scripts.js, link variables, fix js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    $(".person1").text("blah blah");
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput = $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
 
 
     event.preventDefault();
